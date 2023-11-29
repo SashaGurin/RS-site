c688f5d14b171dae2ad2bc272bb9beed06766fbc
Sasha <55543771+SashaGurin@users.noreply.github.com>
2023-11-29 22:30:50 +0300

    add js

---
+++ JS/input-autocom.js
@@ -0,0 +1,10 @@
+const autoCompleteJS = new autoComplete({
+    placeHolder: "Search for cantry...",
+    data: {
+        src: ["Sauce - Thousand Island", "Wild Boar - Tenderloin", "Goat - Whole Cut"]
+    },
+    resultItem: {
+        highlight: true,
+    }
+});
+
